ca10bba964bf45e495c2d1051197c4c551b8b675
driesdroesbeke <dries.droesbeke@digipolis.be>
2016-01-11 19:54:03 +0100

    update vagrant bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 # Downloads the package lists from the repositories
 # and "updates" them to get information on the newest
 # versions of packages and their dependencies
-apt-get update
+
 
 # Install curl
 # -----------
@@ -13,20 +13,24 @@ apt-get install -y curl
 
 # Install Node js 5.3
 # -------------------
-curl -sL https://deb.nodesource.com/setup_5.3 | sudo bash -
-sudo apt-get install -y nodejs-legacy
-sudo apt-get install -y npm
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo -E bash -
+apt-get update
+
+#sudo apt-get install -y nodejs-legacy
+apt-get install -y nodejs
+apt-get install -y npm
+
 
 # Upgrade NPM
 # -----------
-npm update -g npm
+#sudo npm update -g npm
 
 # Install Bower
 # -------------
-npm install -g bower
+sudo npm install -g bower
 
 # Install Grunt
 # -------------
-npm install -g grunt-cli
+sudo npm install -g grunt-cli
 
 
